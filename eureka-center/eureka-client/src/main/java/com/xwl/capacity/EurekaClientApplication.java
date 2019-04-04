1b2a141965c3eab759ba50cf362691a565bee198
xiewanlin <xiewanlin@dafy.com>
2019-04-04 14:14:20 +0800

    add hystrix

--- eureka-center/eureka-client/src/main/java/com/xwl/capacity/EurekaClientApplication.java
+++ eureka-center/eureka-client/src/main/java/com/xwl/capacity/EurekaClientApplication.java
@@ -1,14 +1,52 @@
 package com.xwl.capacity;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableEurekaClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
+
+    @Value("${user.name}")
+    private String name;
+
+    @RequestMapping("/getName")
+    @HystrixCommand(fallbackMethod = "getNameError")
+    public String getName(){
+        return "hi,my name is "+name;
+    }
+
+    public String getNameError(){
+        return "getName method fail!";
+    }
+
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/hystrix.stream");
+        return registration;
+    }
+
 }
